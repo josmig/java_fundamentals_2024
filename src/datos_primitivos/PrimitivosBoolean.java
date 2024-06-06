aac17a6b771edf87fdbd64317e4db2cd440c1f3f
josmig <miguepantigoso@gmail.com>
2024-06-06 11:31:19 -0500

    java fundamentlas

---
+++ src/datos_primitivos/PrimitivosBoolean.java
@@ -0,0 +1,14 @@
+package datos_primitivos;
+
+public class PrimitivosBoolean {
+    
+    public static void main(String []args){
+        
+        Boolean datoLogico= true;
+        System.out.println("datoLogico = " + datoLogico);
+
+
+
+
+    }
+}
