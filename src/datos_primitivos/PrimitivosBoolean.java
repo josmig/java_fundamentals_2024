933fba6fb9afe96950d8a9a08386b5277d475c59
josmig <miguepantigoso@gmail.com>
2024-06-06 20:24:28 -0500

    sistemas numericos utlizando JOPtionpane

--- src/datos_primitivos/PrimitivosBoolean.java
+++ src/datos_primitivos/PrimitivosBoolean.java
@@ -6,7 +6,18 @@ public class PrimitivosBoolean {
         
         Boolean datoLogico= true;
         System.out.println("datoLogico = " + datoLogico);
+        
+        double b = 97865.43e-3;
+        System.out.println("d = " + b);
 
+        float f = 1.2345e2f;
+        System.out.println("f = " + f);
+
+        datoLogico = b < f;
+        System.out.println("datoLogico = " + datoLogico);
+
+        boolean esIgual = (3-2 == 1);
+        System.out.println("esIgual = " + esIgual);
 
 
 
